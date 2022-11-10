ee0c110d25be203fb5bdf9bc4088bb27b00d87e1
Yasmine13-8 <yasminemrt14@gmail.com>
2022-11-10 17:04:11 +0100

    windows-logo

---
+++ src/app/carre/carre.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-carre',
+  templateUrl: './carre.component.html',
+  styleUrls: ['./carre.component.css']
+})
+export class CarreComponent implements OnInit {
+
+  constructor() { }
+
+
+  public changeColor (color : String){
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/carre/petit-carre/petit-carre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-petit-carre',
+  templateUrl: './petit-carre.component.html',
+  styleUrls: ['./petit-carre.component.css']
+})
+export class PetitCarreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
